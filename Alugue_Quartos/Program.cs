7223ff6b33ec97cffc0f097218d08c946aed5997
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 08:59:11 -0300

    Aluguel de Quartos

---
+++ Alugue_Quartos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Alugue_Quartos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluguel[] quartos = new Aluguel[10];
+
+            Console.Write("Quantos registros deseja fazer?");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<N; i++)
+            {
+                Console.WriteLine("Dados Do " + i + "º Aluguel: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int pos = int.Parse(Console.ReadLine());
+                quartos[pos] = new Aluguel(nome, email);
+            }
+
+            Console.WriteLine("Quartos Ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine(i + ": " + quartos[i]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
